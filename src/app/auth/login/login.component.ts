b5fd349b888e3857150fd32eeea7182bf556d3a9
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2025-03-09 22:15:41 -0400

    Add base login

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+  //authService =
+  router = inject(Router); 
+  login(){
+
+  }
+}
