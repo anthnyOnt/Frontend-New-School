39a32a4ad6ceca6cc700cb9d004aecad05219009
Antonio O <antonio.ontiveros@ucb.edu.bo>
2025-03-17 12:50:24 -0400

    setup, create components

---
+++ src/app/admin/pages/cursos/cursos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-cursos",
+  standalone: true,
+  imports: [],
+  templateUrl: "./cursos.component.html",
+  styleUrl: "./cursos.component.scss",
+})
+export class CursosComponent {}
