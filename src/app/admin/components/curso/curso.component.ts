853f06cb726e0845a08bc742e88a7fd8dbb3422b
Antonio O <antonio.ontiveros@ucb.edu.bo>
2025-03-18 00:51:51 -0400

    add curso, cursos components and curso interface

--- src/app/admin/components/curso/curso.component.ts
+++ src/app/admin/components/curso/curso.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { Curso } from '../../../core/interfaces/curso';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-curso',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './curso.component.html',
   styleUrl: './curso.component.scss'
 })
-export class CursoComponent {
-  
+export class CursoComponent{
+  @Input() curso!: Curso;
 }
