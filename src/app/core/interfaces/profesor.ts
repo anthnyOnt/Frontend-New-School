c9453f39d4b2f3dac03984eee94d602b1eb1888e
Isaac71412079 <117691579+Isaac71412079@users.noreply.github.com>
2025-03-17 22:40:51 -0400

    creacion de interfaz de profesor

---
+++ src/app/core/interfaces/profesor.ts
@@ -0,0 +1,11 @@
+export interface Profesor {
+    id: number;
+    nombre: string;
+    fechaNacimiento: Date;
+    nacionalidad: string;
+    correo: string;
+    telefono: number;
+    direccion: string;
+    rol: string;
+    ci: number;
+}
