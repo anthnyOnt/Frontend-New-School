4f8ac3fa632a6131c1481f42a0446f245affa1e0
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2025-03-18 04:43:15 -0400

    Improve pages design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegistrarEstudianteComponent } from './admin/pages/registrar-estudiante/registrar-estudiante.component'
 import { VerEstudiantesComponent } from './admin/pages/ver-estudiantes/ver-estudiantes.component';
 import { GradosPageComponent } from './admin/pages/grados/grados-page/grados-page.component';
+import { MainComponent } from './admin/pages/main/main.component';
 
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
+    {path: '', component: MainComponent},
     {path: 'registrar', component: RegistrarEstudianteComponent},
     {path: 'estudiantes', component: VerEstudiantesComponent},
     {path: 'grados', component: GradosPageComponent}
