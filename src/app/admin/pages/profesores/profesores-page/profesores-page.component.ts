f839b99c1b565b842dfab33871db6d4c96e4fc9b
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2025-03-27 17:49:12 -0400

    Refactorizado y limpiado de importaciones sin uso

--- src/app/admin/pages/profesores/profesores-page/profesores-page.component.ts
+++ src/app/admin/pages/profesores/profesores-page/profesores-page.component.ts
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-profesores-page',
   standalone: true,
-  imports: [ProfesorComponent, CommonModule, ProfesorAddComponent, FormsModule,RouterLink],
+  imports: [ CommonModule, ProfesorAddComponent, FormsModule,RouterLink],
   templateUrl: './profesores-page.component.html',
   styleUrls: ['./profesores-page.component.scss']
 })
