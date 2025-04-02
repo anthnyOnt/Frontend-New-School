559a1cebfb391487aec4a52f6ecd68750f0708e5
Antonio O <antonio.ontiveros@ucb.edu.bo>
2025-04-02 01:01:48 -0400

    create contenido components

---
+++ src/app/admin/pages/contenidos/contenido-form/contenido-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contenido-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './contenido-form.component.html',
+  styleUrl: './contenido-form.component.scss'
+})
+export class ContenidoFormComponent {
+
+}
