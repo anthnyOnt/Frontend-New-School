84572792ad5842d31d247f2b404e65f71c279967
Antonio O <antonio.ontiveros@ucb.edu.bo>
2025-04-22 15:35:54 -0400

    create tarea service, add tareas view

---
+++ src/app/estudiante/pages/tarea-page/tarea-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarea-page.component.html',
+  styleUrl: './tarea-page.component.scss'
+})
+export class TareaPageComponent {
+
+}
