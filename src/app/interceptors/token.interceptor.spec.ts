a2109ddd0ab0b01f31ff8057682edf7b48b6dcd9
Antonio O <antonio.ontiveros@ucb.edu.bo>
2025-04-24 19:57:38 -0400

    fix cursos.component and interceptor specs

---
+++ src/app/interceptors/token.interceptor.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpInterceptorFn, HttpRequest, HttpHandler} from '@angular/common/http';
+
+import { TokenInterceptor } from './token.interceptor';
+
+describe('TokenInterceptor', () => {
+  let interceptor: TokenInterceptor;
+  
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    interceptor = new TokenInterceptor();
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    expect(interceptor).toBeDefined();
+
+    it('should be created', () => {
+      expect(interceptor).toBeTruthy();
+    });
+  })
+})
