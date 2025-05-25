95d152ce85c779c14f599e023e65eb8969edcb7e
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2025-05-25 08:50:58 -0400

    Update html form

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api', // URL de tu backend
-    useMockData: true // Cambiar a false para usar el backend real
+    apiUrl: 'http://localhost:3000/api/v1', // URL de tu backend
+    useMockData: false // Cambiar a false para usar el backend real
   };
