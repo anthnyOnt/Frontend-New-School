21affbf04fa897a1f8260bfeb4fae595db848690
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2025-05-29 15:52:54 -0400

    Se realizo un carrusel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; 
+import { ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,20 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  imagenes: string[] = [
+    'assets/img2.jpg',
+    'assets/img3.jpg',
+    'assets/img4.jpg'
+  ];
+  
+  indiceActual: number = 0;
+  
+  anterior() {
+    this.indiceActual = (this.indiceActual - 1 + this.imagenes.length) % this.imagenes.length;
+  }
+  
+  siguiente() {
+    this.indiceActual = (this.indiceActual + 1) % this.imagenes.length;
+  }
+  
 }
