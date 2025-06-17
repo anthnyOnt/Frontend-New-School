e82255eccdd8b6a4e08e2fe9a51fe85805290703
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2025-06-17 12:08:09 -0400

    Configuracion a backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://backend-new-school.onrender.com/api/v1', 
-    apiUrl: 'http://localhost:8080/api/v1',
+    apiUrl: 'https://backend-new-school.onrender.com/api/v1', 
+    // apiUrl: 'http://localhost:8080/api/v1',
     useMockData: false 
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api/v1', // URL de tu backend
+    apiUrl: 'https://backend-new-school.onrender.com/api/v1',
+    // apiUrl: 'http://localhost:8080/api/v1', // URL de tu backend
     useMockData: false // Cambiar a false para usar el backend real
   };
