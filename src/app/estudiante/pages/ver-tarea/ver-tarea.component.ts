c7eb8f0b1c3a08605ec4cbce6637f25f1f82ff57
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2025-06-17 11:41:05 -0400

    creacion de componente de ver tarea

---
+++ src/app/estudiante/pages/ver-tarea/ver-tarea.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-tarea',
+  standalone: true,
+  imports: [],
+  templateUrl: './ver-tarea.component.html',
+  styleUrl: './ver-tarea.component.scss'
+})
+export class VerTareaComponent {
+
+}
